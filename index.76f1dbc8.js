c49876f78b1ff89d8b52465a4b4505c3f7d424c2
bilalAlpaslan <m.bilal.alpaslan@gmail.com>
2022-10-01 19:05:06 +0300

    update

---
+++ index.76f1dbc8.js
@@ -0,0 +1,2 @@
+window.onload=()=>{let e=document.querySelector("#photoFix"),o=document.querySelector("#photoFixM");screen.width<600?(e.style.display="none",o.style.display="block"):(e.style.display="block",o.style.display="none")};
+//# sourceMappingURL=index.76f1dbc8.js.map
--- index.f18277b2.js
+++
@@ -1,2 +0,0 @@
-window.onload=()=>{let t=document.querySelector("#photoFix");screen.width<600?t.setAttribute("src","assets/images/aboutM.jpg"):t.setAttribute("src","assets/images/about.jpg")};
-//# sourceMappingURL=index.f18277b2.js.map
