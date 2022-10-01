9fa0d619fbc4d4004ffdaf48515a39e7d8e3d436
bilalAlpaslan <m.bilal.alpaslan@gmail.com>
2022-10-01 09:38:33 +0300

    update

---
+++ index.f18277b2.js
@@ -0,0 +1,2 @@
+window.onload=()=>{let t=document.querySelector("#photoFix");screen.width<600?t.setAttribute("src","assets/images/aboutM.jpg"):t.setAttribute("src","assets/images/about.jpg")};
+//# sourceMappingURL=index.f18277b2.js.map
