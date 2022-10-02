5499c002975ce3ce126e4de9f7c89245ba1052ee
bilalAlpaslan <m.bilal.alpaslan@gmail.com>
2022-10-02 13:43:09 +0300

    update

--- index.76f1dbc8.js
+++
@@ -1,2 +0,0 @@
-window.onload=()=>{let e=document.querySelector("#photoFix"),o=document.querySelector("#photoFixM");screen.width<600?(e.style.display="none",o.style.display="block"):(e.style.display="block",o.style.display="none")};
-//# sourceMappingURL=index.76f1dbc8.js.map
